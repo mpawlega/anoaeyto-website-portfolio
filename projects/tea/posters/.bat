e800ed726e7553a570ab53dd9c600606a93534ca
Matilda Pawlega <matildapaw@gmail.com>
2026-02-19 20:48:41 -0500

    compression 2

--- projects/tea/posters/.bat
+++ projects/tea/posters/.bat
@@ -5,7 +5,7 @@ set DIR=C:\Users\Matilda\Documents\GitHub\anoaeyto-website-portfolio\projects\te
 
 for /r "%DIR%" %%f in (*.jpg *.jpeg *.JPG *.JPEG) do (
     echo Compressing: %%f
-    magick "%%f" -quality 75 "%%f"
+    magick "%%f" -resize "2560x2560>" -quality 60 -strip "%%f"
 )
 
 echo Done!
