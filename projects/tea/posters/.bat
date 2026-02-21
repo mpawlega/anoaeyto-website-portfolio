f14c67962ef2e844cbde717fa834afc7d07a0218
Matilda Pawlega <matildapaw@gmail.com>
2026-02-21 18:16:33 -0500

    Updates 2/21/2026 1

--- projects/tea/posters/.bat
+++ projects/tea/posters/.bat
@@ -1,11 +1,8 @@
 @echo off
-setlocal enabledelayedexpansion
 
-set DIR=C:\Users\Matilda\Documents\GitHub\anoaeyto-website-portfolio\projects\tea\posters
-
-for /r "%DIR%" %%f in (*.jpg *.jpeg *.JPG *.JPEG) do (
+for %%f in (*.jpg *.jpeg *.JPG *.JPEG) do (
     echo Compressing: %%f
-    magick "%%f" -resize "2560x2560>" -quality 60 -strip "%%f"
+    sharp -i "%%f" -o "%%f" --quality 75 resize 1600 2000 --withoutEnlargement
 )
 
 echo Done!
